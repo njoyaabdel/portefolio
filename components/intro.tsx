3adeb8c5f3d96cf5c1c4589abb2ec4c7675ccbc2
starntiti <njoyaabdelkarim404@gmail.com>
2025-06-14 06:50:00 +0100

    fix pushing

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,7 +11,8 @@ export default function Intro() {
     <section>
       <div className="flex items-center justify-center">
         <div className="relative">
-            <Image src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+          <motion.div initial={{opacity:0,scale:0}} animate={{opacity:1,scale:1}}>
+          <Image src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
              alt="Ricardo portrait"
              width="192"
              height="192"
@@ -21,6 +22,8 @@ export default function Intro() {
              <span className="absolute bottom-0 right-1 text-4xl ">
                  👋🏻                                
              </span>
+          </motion.div>
+           
         </div>
       </div>
     </section>
