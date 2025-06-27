d561f9bd4c0438caec0f95b3c2b67525c28e1328
starntiti <njoyaabdelkarim404@gmail.com>
2025-06-27 05:25:57 +0100

    adding section skill

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ActiveSectionContexteProvider from "@/context/active-section-contexte";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,16 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
-        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]">
-        </div>
-         <div className="bg-[#dbd7fb] -z-20 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
-        </div>
-        <Header/>
-        {children}
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-400 text-gray-950 relative pt-28 sm:pt-36`}
+      >
+        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] -z-20 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <ActiveSectionContexteProvider>
+          <Header />
+          {children}
+        </ActiveSectionContexteProvider>
       </body>
     </html>
   );
 }
-   
---
+++ components/skills.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Skills() {
+  const { ref } = useSectionInView("Skills");
+
+  return (
+    <section
+      id="skills"
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading>My Skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="bg-white border-black rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+            key={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
